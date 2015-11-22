221573d2f21084222d8e19bf5388dfbde412b0f6
gopal <gopsonly4u@gmail.com>
2015-11-22 16:56:37 +0530

    @Gopal 22/11/2015 5:00pm Addition of Dialog Box for SignUp/Login

--- PROJECT01/WebContent/view/home.controller.js
+++ PROJECT01/WebContent/view/home.controller.js
@@ -1,8 +1,6 @@
-sap.ui
-		.controller(
+sap.ui.controller(
 				"b6.help.view.home",
 				{
-
 					/**
 					 * Called when a controller is instantiated and its View
 					 * controls (if available) are already created. Can be used
@@ -11,42 +9,92 @@ sap.ui
 					 * 
 					 * @memberOf view.home
 					 */
-					 onInit: function() {
-					
+					onInit : function() {
+
 					},
-					onAddressTyped: function(oEvent){
+					onAddressTyped : function(oEvent) {
 						var sTerm = oEvent.getParameter("suggestValue");
 						myAddress = oEvent.getSource();
 						myview = this.getView();
-						if (sTerm.length >= 8){
+						if (sTerm.length >= 8) {
 							var aFilters = [];
-						    if (sTerm) {	    
-						    // google place URL for Ajax call
-						   	var url = "proxy/http/maps.googleapis.com/maps/api/geocode/json?address=" + sTerm + "&sensor=false"			    
-						     var oModel1 = new sap.ui.model.json.JSONModel(); 			     
-						     //Ajax Call Google Place API with Callback function and JSONP data type     
-					          $  
-					                              .ajax({  
-					                                        url : url,  
-					                                        jsonpCallback : 'getJSON',  
-					                                        contentType : "application/json",  
-					                                        dataType : 'json',  
-					                                        success : function(data, textStatus, jqXHR) {  
-					                                         // set formatted address Json file to Odata Modle        
-					                                        	oModel1.setData(data);  
-					                                                  //sap.ui.getCore().setModel(oModel1);
-					                                        	myview.setModel(oModel1);
-					                                           // Binding formatted address with suggestion items 
-					                                                  
-					                                                  aFilters.push(new sap.ui.model.Filter("formatted_address", sap.ui.model.FilterOperator.Contains, ""));
-					                                		      //    oEvent.getSource().getBinding("suggestionItems").filter(aFilters);
-					                                          //      console.log("Source");
-					                                         //  myAddress.getBindingContext();
-					                                                  myAddress.getBinding("suggestionItems").filter(aFilters);
-					                                        }
-					                              });
-					          	          
-						};    
-					};
+							if (sTerm) {
+								// google place URL for Ajax call
+								var url = "proxy/http/maps.googleapis.com/maps/api/geocode/json?address="
+										+ sTerm + "&sensor=false"
+								var oModel1 = new sap.ui.model.json.JSONModel();
+								// Ajax Call Google Place API with Callback
+								// function and JSONP data type
+								$
+										.ajax({
+											url : url,
+											jsonpCallback : 'getJSON',
+											contentType : "application/json",
+											dataType : 'json',
+											success : function(data,
+													textStatus, jqXHR) {
+												// set formatted
+												// address Json
+												// file to Odata
+												// Modle
+												oModel1.setData(data);
+												// sap.ui.getCore().setModel(oModel1);
+												myview.setModel(oModel1);
+												// Binding
+												// formatted
+												// address with
+												// suggestion
+												// items
+
+												aFilters
+														.push(new sap.ui.model.Filter(
+																"formatted_address",
+																sap.ui.model.FilterOperator.Contains,
+																""));
+												// oEvent.getSource().getBinding("suggestionItems").filter(aFilters);
+												// console.log("Source");
+												// myAddress.getBindingContext();
+												myAddress.getBinding(
+														"suggestionItems")
+														.filter(aFilters);
+											}
+										});
+
+							}
+							;
+						}
+						;
 					},
-				});// },
+
+					login : function() {
+
+						this.onOpenDialog();
+
+					},
+
+					_getDialog : function() {
+
+						// create dialog lazily
+
+						if (!this._oDialog) {
+
+							// create dialog via fragment factory
+
+							this._oDialog = sap.ui
+									.xmlfragment("fragment.login");
+
+							// connect dialog to view (models, lifecycle)
+
+							this.getView().addDependent(this._oDialog);
+
+						}
+
+						return this._oDialog;
+
+					},
+
+					onOpenDialog : function() {
+						this._getDialog().open();
+					}
+
+				});
